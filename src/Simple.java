9090245b4a471b876b0f96d73aa88a6c9a5b581b
Anwar Faiz <mfaiz@expedia.com>
2017-05-13 12:39:50 +0530

    first commit

---
+++ src/Simple.java
@@ -0,0 +1,18 @@
+public class Simple{
+	public static void main(String... args){
+		System.out.println("Hi World!");
+		
+		String arabicText = "اسمي أنور";
+		String banglaText = "আমার নাম আনোয়ার";
+		String urduText = "میرا نام انور ہے";
+		String hindiText = "मेरा नाम अनवर है";
+		String frenchText = "Je m'appelle Anwar";
+		
+		System.out.println(arabicText);
+		System.out.println(banglaText);
+		System.out.println(urduText);
+		System.out.println(hindiText);
+		System.out.println(frenchText);		
+	}
+}
+
